6d396f5fa45fb6e78f1cba0c29e10b90a96aa1de
WTYJack <wtyjack@gmail.com>
2015-04-12 16:44:22 -0400

    Spark framework

---
+++ my-ir-app/src/main/java/myirapp/App.java
@@ -0,0 +1,72 @@
+package myirapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.velocity.*;
+
+
+import static spark.Spark.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	port(2345);
+    	staticFileLocation("/");
+    	
+    	get("/hello", (request, response) -> {
+    		Map<String, Object> attributes = new HashMap<>();
+            attributes.put("hello", 1111);
+
+            // The hello.ftl file is located in directory:
+            // src/test/resources/spark/template/freemarker
+            return new ModelAndView(attributes, "hello.wm");
+        }, new VelocityTemplateEngine());
+    	
+        post("/hello", (request, response) ->
+            "Hello World: " + request.body()
+        );
+        
+
+        get("/private", (request, response) -> {
+            response.status(401);
+            return "Go Away!!!";
+        });
+
+        get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+
+        get("/news/:section", (request, response) -> {
+            response.type("text/xml");
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>";
+        });
+
+        get("/protected", (request, response) -> {
+            halt(403, "I don't think so!!!");
+            return null;
+        });
+
+        get("/redirect", (request, response) -> {
+            response.redirect("/news/world");
+            return null;
+        });
+
+        get("/", (request, response) -> {
+    		Map<String, Object> attributes = new HashMap<>();
+            //attributes.put("hello", 1111);
+
+            // The hello.ftl file is located in directory:
+            // src/test/resources/spark/template/freemarker
+            return new ModelAndView(attributes, "index.html");
+        }, new VelocityTemplateEngine());
+        
+        post("/", (request, response) ->
+        request.body()
+        );
+        
+    }
+}
