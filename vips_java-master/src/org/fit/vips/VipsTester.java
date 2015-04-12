08fb1545b1213b4f99c0e84d8e7c9ece916b1e52
ezhuang <ezhuang@purdue.edu>
2015-04-12 14:39:56 -0400

    Vips part

---
+++ vips_java-master/src/org/fit/vips/VipsTester.java
@@ -0,0 +1,47 @@
+/*
+ * Tomas Popela, 2012
+ * VIPS - Visual Internet Page Segmentation
+ * Module - VipsTester.java
+ */
+
+package org.fit.vips;
+
+/**
+ * VIPS API example application.
+ * @author Tomas Popela
+ *
+ */
+public class VipsTester {
+
+	/**
+	 * Main function
+	 * @param args Internet address of web page.
+	 */
+	public static void main(String args[])
+	{
+		// we've just one argument - web address of page
+		if (args.length != 1)
+		{
+			System.err.println("We've just only one argument - web address of page!");
+			System.exit(0);
+		}
+
+		String url = args[0];
+
+		try
+		{
+			Vips vips = new Vips();
+			// disable graphics output
+			vips.enableGraphicsOutput(false);
+			// disable output to separate folder (no necessary, it's default value is false)
+			vips.enableOutputToFolder(false);
+			// set permitted degree of coherence
+			vips.setPredefinedDoC(8);
+			// start segmentation on page
+			vips.startSegmentation(url);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
